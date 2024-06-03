2abe7dc26a5ab02efffbafa3a55ba1d5e711a93b
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-06-03 14:12:06 +0700

    [UPDATE] setup prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image';
+import Image from 'next/image'
 
 export default function Home() {
-    return (
-        <main>
-            <h2 className="font-normal">huynhminhhai</h2>
-        </main>
-    );
+  return (
+    <main>
+      <h2 className='font-normal'>huynhminhhai</h2>
+    </main>
+  )
 }
